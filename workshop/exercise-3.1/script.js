0930a4dc58f4d6cc7617e78be7d4ece05fb2f6fd
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-05-03 21:59:17 -0400

    completed 3-1

---
+++ workshop/exercise-3.1/script.js
@@ -0,0 +1,148 @@
+let body = document.querySelector("body");
+let buttonDiv = document.createElement("buttons");
+body.appendChild(buttonDiv);
+
+let button1 = document.createElement("button");
+let button2 = document.createElement("button");
+let button3 = document.createElement("button");
+let button4 = document.createElement("button");
+let button5 = document.createElement("button");
+let button6 = document.createElement("button");
+let button7 = document.createElement("button");
+let button8 = document.createElement("button");
+let button9 = document.createElement("button");
+let button10 = document.createElement("button");
+let button11 = document.createElement("button");
+let button12 = document.createElement("button");
+let button13 = document.createElement("button");
+let button14 = document.createElement("button");
+let button15 = document.createElement("button");
+let button16 = document.createElement("button");
+let button17 = document.createElement("button");
+let button18 = document.createElement("button");
+let button19 = document.createElement("button");
+let button20 = document.createElement("button");
+
+buttonDiv.appendChild(button1);
+buttonDiv.appendChild(button2);
+buttonDiv.appendChild(button3);
+buttonDiv.appendChild(button4);
+buttonDiv.appendChild(button5);
+buttonDiv.appendChild(button6);
+buttonDiv.appendChild(button7);
+buttonDiv.appendChild(button8);
+buttonDiv.appendChild(button9);
+buttonDiv.appendChild(button10);
+buttonDiv.appendChild(button11);
+buttonDiv.appendChild(button12);
+buttonDiv.appendChild(button13);
+buttonDiv.appendChild(button14);
+buttonDiv.appendChild(button15);
+buttonDiv.appendChild(button16);
+buttonDiv.appendChild(button17);
+buttonDiv.appendChild(button18);
+buttonDiv.appendChild(button19);
+buttonDiv.appendChild(button20);
+
+button1.innerText = 1;
+button2.innerText = 2;
+button3.innerText = 3;
+button4.innerText = 4;
+button5.innerText = 5;
+button6.innerText = 6;
+button7.innerText = 7;
+button8.innerText = 8;
+button9.innerText = 9;
+button10.innerText = 10;
+button11.innerText = 11;
+button12.innerText = 12;
+button13.innerText = 13;
+button14.innerText = 14;
+button15.innerText = 15;
+button16.innerText = 16;
+button17.innerText = 17;
+button18.innerText = 18;
+button19.innerText = 19;
+button20.innerText = 20;
+
+button1.id = "button1";
+button2.id = "button2";
+button3.id = "button3";
+button4.id = "button4";
+button5.id = "button5";
+button6.id = "button6";
+button7.id = "button7";
+button8.id = "button8";
+button9.id = "button9";
+button10.id = "button10";
+button11.id = "button11";
+button12.id = "button12";
+button13.id = "button13";
+button14.id = "button14";
+button15.id = "button15";
+button16.id = "button16";
+button17.id = "button17";
+button18.id = "button18";
+button19.id = "button19";
+button20.id = "button20";
+
+button1.addEventListener("click", function () {
+  document.querySelector("#button1").style.backgroundColor = "green";
+});
+button2.addEventListener("click", function () {
+  document.querySelector("#button2").style.backgroundColor = "green";
+});
+button3.addEventListener("click", function () {
+  document.querySelector("#button3").style.backgroundColor = "green";
+});
+button4.addEventListener("click", function () {
+  document.querySelector("#button4").style.backgroundColor = "green";
+});
+button5.addEventListener("click", function () {
+  document.querySelector("#button5").style.backgroundColor = "green";
+});
+button6.addEventListener("click", function () {
+  document.querySelector("#button6").style.backgroundColor = "green";
+});
+button7.addEventListener("click", function () {
+  document.querySelector("#button7").style.backgroundColor = "green";
+});
+button8.addEventListener("click", function () {
+  document.querySelector("#button8").style.backgroundColor = "green";
+});
+button9.addEventListener("click", function () {
+  document.querySelector("#button9").style.backgroundColor = "green";
+});
+button10.addEventListener("click", function () {
+  document.querySelector("#button10").style.backgroundColor = "green";
+});
+button11.addEventListener("click", function () {
+  document.querySelector("#button11").style.backgroundColor = "green";
+});
+button12.addEventListener("click", function () {
+  document.querySelector("#button12").style.backgroundColor = "green";
+});
+button13.addEventListener("click", function () {
+  document.querySelector("#button13").style.backgroundColor = "green";
+});
+button14.addEventListener("click", function () {
+  document.querySelector("#button14").style.backgroundColor = "green";
+});
+button15.addEventListener("click", function () {
+  document.querySelector("#button15").style.backgroundColor = "green";
+});
+button16.addEventListener("click", function () {
+  document.querySelector("#button16").style.backgroundColor = "green";
+});
+button17.addEventListener("click", function () {
+  document.querySelector("#button17").style.backgroundColor = "green";
+});
+button18.addEventListener("click", function () {
+  document.querySelector("#button18").style.backgroundColor = "green";
+});
+button19.addEventListener("click", function () {
+  document.querySelector("#button19").style.backgroundColor = "green";
+});
+button20.addEventListener("click", function () {
+  document.querySelector("#button20").style.backgroundColor = "green";
+});
