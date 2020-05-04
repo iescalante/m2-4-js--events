75fd068b5c58196882cd2878ee9c9c136421a6d2
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-05-03 22:18:44 -0400

    completed 3-2

---
+++ workshop/exercise-3.2/script.js
@@ -0,0 +1,148 @@
+let body = document.querySelector("body");
+let buttonDiv = document.createElement("buttons");
+body.appendChild(buttonDiv);
+
+let button1 = document.createElement("button");
+let button2 = document.createElement("button");
+let button3 = document.createElement("button");
+let button4 = document.createElement("button");
+let button5 = document.createElement("button");
+let button6 = document.createElement("button");
+let button7 = document.createElement("button");
+let button8 = document.createElement("button");
+let button9 = document.createElement("button");
+let button10 = document.createElement("button");
+let button11 = document.createElement("button");
+let button12 = document.createElement("button");
+let button13 = document.createElement("button");
+let button14 = document.createElement("button");
+let button15 = document.createElement("button");
+let button16 = document.createElement("button");
+let button17 = document.createElement("button");
+let button18 = document.createElement("button");
+let button19 = document.createElement("button");
+let button20 = document.createElement("button");
+
+buttonDiv.appendChild(button1);
+buttonDiv.appendChild(button2);
+buttonDiv.appendChild(button3);
+buttonDiv.appendChild(button4);
+buttonDiv.appendChild(button5);
+buttonDiv.appendChild(button6);
+buttonDiv.appendChild(button7);
+buttonDiv.appendChild(button8);
+buttonDiv.appendChild(button9);
+buttonDiv.appendChild(button10);
+buttonDiv.appendChild(button11);
+buttonDiv.appendChild(button12);
+buttonDiv.appendChild(button13);
+buttonDiv.appendChild(button14);
+buttonDiv.appendChild(button15);
+buttonDiv.appendChild(button16);
+buttonDiv.appendChild(button17);
+buttonDiv.appendChild(button18);
+buttonDiv.appendChild(button19);
+buttonDiv.appendChild(button20);
+
+button1.innerText = 1;
+button2.innerText = 2;
+button3.innerText = 3;
+button4.innerText = 4;
+button5.innerText = 5;
+button6.innerText = 6;
+button7.innerText = 7;
+button8.innerText = 8;
+button9.innerText = 9;
+button10.innerText = 10;
+button11.innerText = 11;
+button12.innerText = 12;
+button13.innerText = 13;
+button14.innerText = 14;
+button15.innerText = 15;
+button16.innerText = 16;
+button17.innerText = 17;
+button18.innerText = 18;
+button19.innerText = 19;
+button20.innerText = 20;
+
+button1.id = "button1";
+button2.id = "button2";
+button3.id = "button3";
+button4.id = "button4";
+button5.id = "button5";
+button6.id = "button6";
+button7.id = "button7";
+button8.id = "button8";
+button9.id = "button9";
+button10.id = "button10";
+button11.id = "button11";
+button12.id = "button12";
+button13.id = "button13";
+button14.id = "button14";
+button15.id = "button15";
+button16.id = "button16";
+button17.id = "button17";
+button18.id = "button18";
+button19.id = "button19";
+button20.id = "button20";
+
+button1.addEventListener("click", function () {
+  button1.classList.toggle("colorgreen");
+});
+button2.addEventListener("click", function () {
+  button2.classList.toggle("colorgreen");
+});
+button3.addEventListener("click", function () {
+  button3.classList.toggle("colorgreen");
+});
+button4.addEventListener("click", function () {
+  button4.classList.toggle("colorgreen");
+});
+button5.addEventListener("click", function () {
+  button5.classList.toggle("colorgreen");
+});
+button6.addEventListener("click", function () {
+  button6.classList.toggle("colorgreen");
+});
+button7.addEventListener("click", function () {
+  button7.classList.toggle("colorgreen");
+});
+button8.addEventListener("click", function () {
+  button8.classList.toggle("colorgreen");
+});
+button9.addEventListener("click", function () {
+  button9.classList.toggle("colorgreen");
+});
+button10.addEventListener("click", function () {
+  button10.classList.toggle("colorgreen");
+});
+button11.addEventListener("click", function () {
+  button11.classList.toggle("colorgreen");
+});
+button12.addEventListener("click", function () {
+  button12.classList.toggle("colorgreen");
+});
+button13.addEventListener("click", function () {
+  button13.classList.toggle("colorgreen");
+});
+button14.addEventListener("click", function () {
+  button14.classList.toggle("colorgreen");
+});
+button15.addEventListener("click", function () {
+  button15.classList.toggle("colorgreen");
+});
+button16.addEventListener("click", function () {
+  button16.classList.toggle("colorgreen");
+});
+button17.addEventListener("click", function () {
+  button17.classList.toggle("colorgreen");
+});
+button18.addEventListener("click", function () {
+  button18.classList.toggle("colorgreen");
+});
+button19.addEventListener("click", function () {
+  button19.classList.toggle("colorgreen");
+});
+button20.addEventListener("click", function () {
+  button20.classList.toggle("colorgreen");
+});
